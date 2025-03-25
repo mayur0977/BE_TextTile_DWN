1a0e678b9e8485fa20498399faad572b7b21fc20
Mayur B Patel <mayurpatel98765@gmail.com>
2025-03-25 17:41:39 +0530

    Add : add product and list product

--- src/products/model/product.model.ts
+++ src/products/model/product.model.ts
@@ -2,107 +2,116 @@ import { ITextileProduct } from './product.types';
 import mongoose from 'mongoose';
 
 // Define the schema for the textile product
-const textileProductSchema = new mongoose.Schema<ITextileProduct>({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  brand: {
-    type: String,
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  unit: {
-    type: String,
-    required: true,
-  },
-  image_url: {
-    type: String,
-    required: true,
-  },
-  stock_quantity: {
-    type: Number,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
-  dimensions: {
-    length: {
+const textileProductSchema = new mongoose.Schema<ITextileProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    price: {
       type: Number,
       required: true,
     },
-    width: {
+    unit: {
+      type: String,
+      required: true,
+    },
+    image_url: {
+      type: String,
+      required: true,
+    },
+    stock_quantity: {
       type: Number,
       required: true,
     },
-    height: {
+    weight: {
       type: Number,
-      required: false,
+      required: true,
     },
-  },
-  created_at: {
-    type: Date,
-    default: new Date(),
-  },
-  updated_at: {
-    type: Date,
-    default: new Date(),
-  },
-  tags: [
-    {
+    dimensions: {
+      length: {
+        type: Number,
+        required: true,
+      },
+      width: {
+        type: Number,
+        required: true,
+      },
+      height: {
+        type: Number,
+        required: false,
+      },
+    },
+    created_at: {
+      type: Date,
+      default: new Date(),
+    },
+    updated_at: {
+      type: Date,
+      default: new Date(),
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    composition: {
+      type: String,
+      required: true,
+    },
+    suitable_for: [
+      {
+        type: String,
+      },
+    ],
+    care_instructions: {
       type: String,
     },
-  ],
-  composition: {
-    type: String,
-    required: true,
-  },
-  suitable_for: [
-    {
+    origin: {
       type: String,
     },
-  ],
-  care_instructions: {
-    type: String,
+    sustainability_rating: {
+      type: String,
+    },
+    texture: {
+      type: String,
+    },
+    fire_retardant: {
+      type: Boolean,
+      default: false,
+    },
+    water_resistant: {
+      type: Boolean,
+      default: false,
+    },
+    pattern: {
+      type: String,
+    },
   },
-  origin: {
-    type: String,
-  },
-  sustainability_rating: {
-    type: String,
-  },
-  texture: {
-    type: String,
-  },
-  fire_retardant: {
-    type: Boolean,
-    default: false,
-  },
-  water_resistant: {
-    type: Boolean,
-    default: false,
-  },
-  pattern: {
-    type: String,
-  },
+  { versionKey: false },
+);
+
+textileProductSchema.pre('find', function (next) {
+  this.select('-__v');
+  next();
 });
+
 // Middleware to update the 'updatedAt' field before each save
 textileProductSchema.pre('save', function (next) {
   this.updated_at = new Date();
